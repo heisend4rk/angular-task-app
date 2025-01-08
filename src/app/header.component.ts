7f1d9c9d14cbfca49fa9799be551b7b812d3667d
heisend4rk <keyproco.rd@gmail.com>
2025-01-08 10:12:08 +0100

    init angular app

---
+++ src/app/header.component.ts
@@ -0,0 +1,11 @@
+
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-header',
+  standalone: true, // by default set to true
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css' 
+})
+
+export class HeaderComponent {}
